9b71723630dcb31839a82b22bcda2d7ba5b7507e
okh-engineer <ohojimirzayev@gmail.com>
2024-03-11 01:36:36 +0500

    Add models

--- warehouse_app/models.py
+++ warehouse_app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    product_name = models.CharField(max_length=100)
+    product_code = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.product_name
+
+class Material(models.Model):
+    material_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.material_name
+
+class ProductMaterial(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product_materials')
+    material = models.ForeignKey(Material, on_delete=models.CASCADE)
+    quantity = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.product.product_name} - {self.material.material_name}"
+
+class Warehouse(models.Model):
+    material = models.ForeignKey(Material, on_delete=models.CASCADE)
+    remainder = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.material.material_name} - {self.remainder} - {self.price}"
+
+
