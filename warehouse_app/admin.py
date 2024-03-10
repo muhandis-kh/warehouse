03e829ed89be8305f83322cfe36adf2abc8692d8
okh-engineer <ohojimirzayev@gmail.com>
2024-03-11 01:36:26 +0500

    Register models for admin

--- warehouse_app/admin.py
+++ warehouse_app/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Product, Material, ProductMaterial, Warehouse
 
 # Register your models here.
+@admin.register(Product)
+class Product(admin.ModelAdmin):
+    list_display = ("product_name", 'product_code')
+    
+@admin.register(Material)
+class Product(admin.ModelAdmin):
+    list_display = ("material_name",)
+    
+@admin.register(ProductMaterial)
+class Product(admin.ModelAdmin):
+    list_display = ("product", 'material', 'quantity')
+    
+@admin.register(Warehouse)
+class Product(admin.ModelAdmin):
+    list_display = ("material", 'remainder', 'price')
